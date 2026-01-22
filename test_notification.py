0d6b48b45f4fe15e8170ebdf5b16235e4f2d25c5
Jo_kabonga <105952503+thiszenon@users.noreply.github.com>
2026-01-22 22:35:03 +0100

    Fix notification message typo and print statement

--- test_notification.py
+++ test_notification.py
@@ -3,8 +3,8 @@ from plyer import notification
 
 notification.notify(
     title="Glint Test",
-    message="If you see this, notifications are working!",
+    message="If i see this, notifications are working!",
     app_name="Glint",
     timeout=10
 )
-print("Notification sent! Check your system tray.")
+print("Notification sent! Check your system .")
